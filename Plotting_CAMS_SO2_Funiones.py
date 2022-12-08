e9bf3c1ec18b68451a6e266b5db8829956b8f7fc
Leonardo <leonardojimenez1990@gmail.com>
2022-12-08 18:10:55 +0000

    actualizar

--- Plotting_CAMS_SO2_Funiones.py
+++ Plotting_CAMS_SO2_Funiones.py
@@ -54,7 +54,7 @@ def creargraficoso2(ds, so2_2003_2020):
 
 
 def leersulphur_dioxide2003_2021CMAS():
-    with xr.open_dataset('sulphur_dioxideEne_dic2003_2020CMAS.nc') as ds:  # sulphur_dioxideEne_dic2003_2020CMAS.nc
+    with xr.open_dataset('sulphur_dioxide2003_2021CMAS.nc') as ds:  # sulphur_dioxideEne_dic2003_2020CMAS.nc
         print(ds)
         # Append the height dimension
         so2_2003_2020 = ds.expand_dims({"height": 9})
